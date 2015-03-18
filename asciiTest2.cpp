28a3fc252696c6edfe3cb309e9a931a6910c1dba
rgomezam <raquel.gomez@cern.ch>
2015-03-18 14:38:56 +0100

    Create asciiTest2.cpp

---
+++ asciiTest2.cpp
@@ -0,0 +1,34 @@
+//============================================================================
+// Name        : asciiTest.cpp
+// Author      : Raquel Gomez
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+
+ //   ofstream: Stream class to write on files
+ //   ifstream: Stream class to read from files
+ //   fstream: Stream class to both read and write from/to files.
+
+
+using namespace std;
+
+char filename[50];
+
+int main() {
+
+	fstream file;
+	cout << "Enter file name to open :: (Write the full path or otherwise it will be created on the current workspace) " << endl;
+	cin.getline(filename, 36);
+
+	file.open(filename, ios_base::out);
+	for (int i = 0; i <= 255; i++)
+		file << i << "\t";
+	file.close();
+	cout << "success" << endl;
+	return 0;
+}
+
